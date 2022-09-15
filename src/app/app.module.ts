1906436b44d6dd225718aff1209dcd75e3ff9640
Florian RENARD <florian.renard86@gmail.com>
2022-09-15 10:31:05 +0200

    Home page creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,17 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
+import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {ToastrModule} from "ngx-toastr";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
+import {MatDialogModule} from "@angular/material/dialog";
+import {HomeModule} from "./home/home.module";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -11,8 +19,16 @@ import {ToastrModule} from "ngx-toastr";
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     BrowserAnimationsModule,
-    ToastrModule.forRoot()
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatButtonModule,
+    MatDialogModule,
+    HomeModule,
+    ToastrModule.forRoot(),
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/constants/app-constants.ts
@@ -0,0 +1,6 @@
+import {environment} from "../../../environments/environment";
+
+export const APP_NAME = 'AM Dashboard';
+export const API_URL = environment.urlApi;
+
+export const COUNTRIES_WITH_REGIONS = ['us', 'br', 'ca', 'au'];
